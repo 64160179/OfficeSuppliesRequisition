0a780371b0659d8a399afb492eca9d24ca244c24
64160179 <64160179@go.buu.ac.th>
2024-09-25 01:06:16 +0700

    fix css && end this day

--- server/routes/ProductRoute.js
+++ server/routes/ProductRoute.js
@@ -7,14 +7,14 @@ import {
     updateProductVisibility,
     deleteProduct
 } from "../controllers/ProductController.js";
-import { verifyUser } from "../middleware/AuthUser.js";
+import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 const router = express.Router();
 
 router.get('/products', verifyUser, getProducts);
 router.get('/products/:id', verifyUser, getProductById);
-router.post('/products', verifyUser, createProduct);
-router.patch('/products/:id', verifyUser, updateProduct);
-router.patch('/products/visibility/:id', verifyUser, updateProductVisibility);
-router.delete('/products/:id', verifyUser, deleteProduct);
+router.post('/products', verifyUser, adminOnly, createProduct);
+router.patch('/products/:id', verifyUser, adminOnly, updateProduct);
+router.patch('/products/visibility/:id', verifyUser, adminOnly, updateProductVisibility);
+router.delete('/products/:id', verifyUser, adminOnly, deleteProduct);
 
 export default router;
